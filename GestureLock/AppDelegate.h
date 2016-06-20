550c682fa3fa818ee9167f3a7e1beadff806127d
zhaoName <zhao1529835@126.com>
2016-06-20 10:52:04 +0800

    Initial Commit

---
+++ GestureLock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GestureLock
+//
+//  Created by zhao on 16/6/20.
+//  Copyright © 2016年 zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GestureLock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GestureLock
+//
+//  Created by zhao on 16/6/20.
+//  Copyright © 2016年 zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
